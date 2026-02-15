b7d42a85914fad064c9ea0da9865e981acf38dcd
Addana24 <pradanaadam4@gmail.com>
2026-02-15 22:39:27 +0700

    fix correct

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('/admin');
+    // Gunakan redirect() untuk mengalihkan ke URL /admin
+    return redirect('/admin');
 });
